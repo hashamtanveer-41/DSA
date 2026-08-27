fb15206df983a89841f8cbccdcfaca353566170e
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-28 01:29:13 +0500

    79. Word Search: Time: 191 ms, Space: 10.5 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/79-word-search/solution.cpp
+++ leetcode/medium/79-word-search/solution.cpp
@@ -8,7 +8,7 @@ bool valid (int i, int j, int n, int m) {
 }
 int x[4] = {-1, 1, 0, 0};
 int y[4] = {0, 0, -1, 1};
-    void findWord(vector<vector<char>>& board,int n, int m, int row, int col,int idx,string target) {
+    void findWord(vector<vector<char>>& board,int n, int m, int row, int col,int idx,const string& target) {
     if (idx==target.size()){result = true;return;}
     char val = board[row][col];
     board[row][col] = '*';
@@ -17,6 +17,7 @@ int y[4] = {0, 0, -1, 1};
         int c = col + y[i];
         if (valid(r, c, n, m) && board[r][c]!='*' && board[r][c]==target[idx]) {
             findWord(board, n, m, r, c, idx+1, target);
+            if (result) return;
         }
     }
     board[row][col] = val;
