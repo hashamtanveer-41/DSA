d8c6be574331ca7ff1536b4e0bbc60b9bd45975f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-28 01:26:58 +0500

    79. Word Search: Time: 323 ms, Space: 11.7 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/79-word-search/solution.cpp
+++ leetcode/medium/79-word-search/solution.cpp
@@ -8,33 +8,29 @@ bool valid (int i, int j, int n, int m) {
 }
 int x[4] = {-1, 1, 0, 0};
 int y[4] = {0, 0, -1, 1};
-    void findWord(vector<vector<char>>& board,int n, int m, vector<vector<int>>& vis, int row, int col,int idx,string target) {
-    if (idx==target.size())result = true;
-    vis[row][col] = 1;
+    void findWord(vector<vector<char>>& board,int n, int m, int row, int col,int idx,string target) {
+    if (idx==target.size()){result = true;return;}
+    char val = board[row][col];
+    board[row][col] = '*';
     for (int i=0; i<4; i++) {
         int r = row + x[i];
         int c = col + y[i];
-        if (valid(r, c, n, m) && vis[r][c]==-1 && board[r][c]==target[idx]) {
-            findWord(board, n, m, vis, r, c, idx+1, target);
+        if (valid(r, c, n, m) && board[r][c]!='*' && board[r][c]==target[idx]) {
+            findWord(board, n, m, r, c, idx+1, target);
         }
     }
-    vis[row][col] = -1;
+    board[row][col] = val;
 }
 bool exist(vector<vector<char>>& board, string word) {
     int n = board.size();
     int m = board[0].size();
-    vector<vector<int>> vis(n);
-    for (int i =0; i<n; i++) {
-        vector<int> t(m, -1);
-        vis[i] = t;
-    }
     for (int i=0; i<n; i++) {
         for (int j=0; j<m; j++) {
             if (board[i][j]==word[0])
-                findWord(board, n, m, vis,i, j, 1, word);
+                findWord(board, n, m,i, j, 1, word);
+                if(result)return true;
         }
     }
-
-    return result;
+    return false;;
 }
 };
