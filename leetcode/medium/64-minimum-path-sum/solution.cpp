f43db57a3152c6309794bbaa1726b404ad594eaf
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-29 03:42:33 +0500

    64. Minimum Path Sum: Time: 0 ms, Space: 15.4 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/64-minimum-path-sum/solution.cpp
+++ leetcode/medium/64-minimum-path-sum/solution.cpp
@@ -1,40 +1,20 @@
 class Solution {
 public:
-bool valid (int i, int j, int n, int m) {
-    if (i<0 || i>=n || j< 0|| j>=m)
-        return false;
-    return true;
-}
     int minPathSum(vector<vector<int>>& grid) {
+    
     int n = grid.size();
     int m = grid[0].size();
-    vector<vector<int>> adj(n);
-    for (int i=0; i<n; i++) {
-        vector<int> t(m, INT_MAX);
-        adj[i] = t;
+    for(int i=1; i<n; i++){
+        grid[i][0] += grid[i-1][0];
     }
-    queue<pair<int, int>> q;
-    q.push({0, 0});
-    adj[0][0] = grid[0][0];
-    int x[2] = {1, 0};
-    int y[2] = {0, 1};
-    while (!q.empty()) {
-        pair<int,int> p = q.front();
-        q.pop();
-        int row = p.first;
-        int column = p.second;
-        int val = adj[row][column];
-        for (int i=0; i<2; i++) {
-            int r  = row + x[i];
-            int c= column + y[i];
-            if (!valid(r, c, n, m))continue;
-            int newSum = val + grid[r][c];
-            if (newSum<adj[r][c]) {
-                adj[r][c] = newSum;
-                q.push({r, c});
-            }
+    for(int i=1; i<m; i++){
+        grid[0][i] += grid[0][i-1];
+    }
+    for (int i =1; i<n; i++) {
+        for (int j=1; j<m; j++) {
+            grid[i][j] = grid[i][j] + min(grid[i-1][j], grid[i][j-1]);
         }
     }
-    return adj[n-1][m-1];
+    return grid[n-1][m-1];
 }
 };
