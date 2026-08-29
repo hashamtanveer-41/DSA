c1a661f322d490c673cbbe99ebe72799084ea876
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-29 22:42:13 +0500

    279. Perfect Squares: Time: 46 ms, Space: 13 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/279-perfect-squares/solution.cpp
+++ leetcode/medium/279-perfect-squares/solution.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-   int findNumSquares (int n, int squares, int currSum,vector<vector<int>>& dp ) {
-   if (currSum>n || squares==0)return 1e8;
-    if (currSum==n)return 0;
-    if(dp[squares][currSum]!=-1)return dp[squares][currSum];
-    int c1 = 1+ findNumSquares(n, squares, currSum+(squares*squares), dp);
-    int c2 = findNumSquares(n, squares-1, currSum, dp);
-    return dp[squares][currSum] = min(c1 ,c2);
-}
+ 
 int numSquares(int n) {
-    int squares = 1;
-    while ((squares*squares)<=n)squares++;
-    squares--;
-    vector<vector<int>> dp(squares+1, vector<int> (n+1, -1));
-    return findNumSquares(n, squares, 0, dp);
+    vector<int> dp(n+1, 1e8);
+    dp[0]=0;
+    for (int i=1; i<=n; i++) {
+        for (int j=1; (j*j)<=i; j++)
+            dp[i] = min(1+dp[i-(j*j)], dp[i]);
+    }
 
+    return dp[n];
 }
 };
