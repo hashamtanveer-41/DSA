cf1da679586b0eca1cbdcaa775bdbb0d6ff55fa7
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-08-30 00:05:54 +0500

    322. Coin Change: Time: 20 ms, Space: 18 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/322-coin-change/solution.cpp
+++ leetcode/medium/322-coin-change/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
-   int findCoinChange(vector<int>& coins, int target, long long currSum, int idx, vector<vector<long long>>& dp) {
-    if (currSum>target || idx==coins.size())return 1e8;
-    if (currSum == target)return 0;
-    if (dp[idx][currSum]!=-1)return dp[idx][currSum];
-    int c3 = 1+findCoinChange(coins, target, currSum+coins[idx], idx, dp);
-    int c2 = findCoinChange(coins, target, currSum , idx+1, dp);
-    return dp[idx][currSum] = min(c2, c3);
-}
 int coinChange(vector<int>& coins, int amount) {
-    vector<vector<long long>> dp(coins.size()+1, vector<long long>(amount+1, -1)); 
-    int ans =  findCoinChange(coins, amount, 0, 0 ,dp);
-    if (ans>=1e8)return -1;
-    return ans;
-}
+   int n = coins.size();
+    vector<int> dp(amount+1, 1e8);
+    dp[0] = 0;
+    for (int i=1; i<=amount; i++) {
+        for (int j = 0; j<n; j++) {
+            if (coins[j]<=i)
+                dp[i] = min(1+dp[i-coins[j]], dp[i]);
+        }
+    }
+
+    return dp[amount]>=1e8?-1:dp[amount];
+    }
 };
