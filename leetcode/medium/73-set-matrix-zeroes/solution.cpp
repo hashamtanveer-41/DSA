68b18e9c94e25061744dbc18f42b339b98bcfc44
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-09-01 23:34:10 +0500

    73. Set Matrix Zeroes: Time: 0 ms, Space: 21 MB - AlgoVault (+2 files) - AlgoVault

--- leetcode/medium/73-set-matrix-zeroes/solution.cpp
+++ leetcode/medium/73-set-matrix-zeroes/solution.cpp
@@ -3,19 +3,30 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
       int n= matrix.size();
     int m = matrix[0].size();
-    vector<bool> row(n, false);
-    vector<bool> col(m, false);
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<m; j++) {
+    bool rowZero = false;
+    bool colZero = false;
+    for(int i=0; i<n; i++)
+        if(matrix[i][0]==0)colZero = true;
+        for(int i=0; i<m; i++)
+        if(matrix[0][i]==0)rowZero = true;
+    for (int i=1; i<n; i++) {
+        for (int j=1; j<m; j++) {
             if (matrix[i][j]==0) {
-                // make its row 0
-                row[i] = true;
-                col[j] = true;
+                matrix[i][0]= 0;
+                matrix[0][j]=0;
             }
         }
     }
-    for (int i=0; i<n; i++)
-        for (int j=0; j<m; j++)
-            if (row[i] || col[j])matrix[i][j]=0;
+    for (int i=1; i<n; i++)
+        for (int j=1; j<m; j++)
+            if (matrix[i][0]==0 || matrix[0][j]==0)matrix[i][j]=0;
+    if(rowZero){
+        for(int i=0; i<m; i++)
+            matrix[0][i]=0;
+    }
+    if(colZero){
+for(int i=0; i<n; i++)
+            matrix[i][0]=0;
+    }
     }
 };
